3de5e04808fc0ea62cd41dbf54f1269e3259d2ce
Mikhail Lazarau (Michaux) <mikhail.lazarau1@gmail.com>
2021-06-07 23:55:19 +0300

    task 1

--- Swift_Task4/ios.stage-task/Exercises/Exercise-1/IntToRoman.swift
+++ Swift_Task4/ios.stage-task/Exercises/Exercise-1/IntToRoman.swift
@@ -2,7 +2,29 @@ import Foundation
 
 public extension Int {
     
-    var roman: String? {
-        nil
-    }
+        var roman: String? {
+            var number = self
+            var romanString = ""
+            
+            guard (1...3999).contains(self) else {
+                   return nil
+               }
+
+            let romanDigits : [String] = ["M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"]
+            let integerDigits : [Int] = [1000,900,500,400,100,90,50,40,10,9,5,4,1]
+
+            var i = 0
+            while number > 0 && i < romanDigits.count {
+                var div = number/integerDigits[i]
+                number = number % integerDigits[i]
+
+                while div != 0 {
+                    romanString += romanDigits[i]
+                    div -= 1
+                }
+                i+=1
+            }
+            return romanString
+            
+        }
 }
